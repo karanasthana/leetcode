355f9d600c76ccd155989f2fc9f7be382e348f29
Karan Asthana <karan@p3infotech.in>
2020-07-25 00:27:03 +0530

    solution to 02May

--- 0534-jewels-and-stones-02MAY2020.java
+++ 0534-jewels-and-stones-02MAY2020.java
@@ -24,6 +24,12 @@ The characters in J are distinct.
 
 class Solution {
     public int numJewelsInStones(String J, String S) {
-        
+        int count=0;
+        for(int i=0; i<S.length(); i++) {
+            if (J.contains(""+S.charAt(i))) {
+                count++;
+            }
+        }
+        return count;
     }
 }
