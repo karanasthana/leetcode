9567db1e024a61b8a0eb929aeaf20d1695fe7d11
Karan Asthana <karan@p3infotech.in>
2020-07-25 13:24:57 +0530

    added solution for 05May

--- 3320-first-unique-character-05MAY2020.java
+++ 3320-first-unique-character-05MAY2020.java
@@ -19,6 +19,18 @@ Note: You may assume the string contains only lowercase English letters.
 
 class Solution {
     public int firstUniqChar(String s) {
-        
+        int arr[] = new int[26];
+        for(int i=0; i<s.length(); i++) {
+            char d = s.charAt(i);
+            arr[d-'a']++;
+        }
+        int index=-1;
+        for(int i=0; i<s.length(); i++) {
+            if (arr[s.charAt(i)-'a'] == 1) {
+                index=i;
+                break;
+            }
+        }
+        return index;
     }
 }
