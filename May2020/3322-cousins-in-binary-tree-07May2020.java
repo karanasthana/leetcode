5ec4a3bc859b271988eb6fe481c1c8136f2f4f02
Karan Asthana <karan@p3infotech.in>
2020-07-26 21:44:24 +0530

    added accepted solution for 7May

--- May2020/3322-cousins-in-binary-tree-07May2020.java
+++ May2020/3322-cousins-in-binary-tree-07May2020.java
@@ -56,8 +56,49 @@ Each node has a unique integer value from 1 to 100.
  *     }
  * }
  */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
+    TreeNode parentX=null;
+    TreeNode parentY=null;
+    int depthX=-1;
+    int depthY=-1;
+
     public boolean isCousins(TreeNode root, int x, int y) {
+        getDepthAndParent(root, x, y, 0, null);
+        return (depthX==depthY && parentX != parentY);
+    }
+    
+    public void getDepthAndParent(TreeNode root, int x, int y, int depth, TreeNode parent) {
+        if (root == null) {
+            return;
+        }
         
+        if (root.val == x) {
+            parentX = parent;
+            depthX = depth;
+        }
+        
+        if (root.val == y) {
+            parentY = parent;
+            depthY = depth;
+        }
+        
+        getDepthAndParent(root.left, x, y, depth+1, root);
+        getDepthAndParent(root.right, x, y, depth+1, root);
     }
 }
