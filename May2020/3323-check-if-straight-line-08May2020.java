dd931b9bcdae6b8a0aa752929d44427da6fb57d2
Karan Asthana <karan@p3infotech.in>
2020-07-28 00:13:02 +0530

    added solution

--- May2020/3323-check-if-straight-line-08May2020.java
+++ May2020/3323-check-if-straight-line-08May2020.java
@@ -21,3 +21,26 @@ coordinates[i].length == 2
 coordinates contains no duplicate point.
 ************************************************************/
 
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+        int[] xy1 = coordinates[0];
+        double slope = findSlope(coordinates[0], coordinates[1]);
+
+        for (int i=2; i<coordinates.length; i++) {
+            if (findSlope(xy1, coordinates[i]) != slope) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public double findSlope(int[] xy1, int[] xy2) {
+        if (xy1[0] == xy2[0]) {
+            return Double.MIN_VALUE;
+        }
+        double deltaY = xy1[1] - xy2[1];
+        double deltaX = xy1[0] - xy2[0];
+        double slope = (deltaY/deltaX);
+        return slope;
+    }
+}
